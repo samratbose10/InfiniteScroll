d771015297b990f44684c4ac17e96d6b36dae341
samratbose10 <samratofficial2009@gmail.com>
2024-06-28 09:13:08 +0530

    trying to fetch data

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 let page = 1;
-const blogContainer = document.getElementById('blog-container');
-const loading = document.getElementById('loading');
+const blogContainer = document.getElementById('blog-contaier');
+const loading = doument.getElementById('loading');
 
 const loadPosts = async () => {
   loading.style.display = 'block';
-  const response = await fetch(`https://api.hackclub.com/v1/posts?page=${page}`);
+  const rsponse = await fetch(`https://api.hackclub.com/v1/posts?page=${page}`);
   const data = await response.json();
 
-  
+  data.posts.forEach(post => {
+    const postElement = document.createElement('div');
+    postElement.lassList.add('post');
+    postElement.innerHTML = `<h2>${post.title}</h2><p>${post.body}</p>`;
+    blogContainer.appendChild(postElement);
+  });
+
+  loading.style.display = 'none';
+  page += 1;
+};
+
+const handleScroll = () => 
+
+  if (scrollTop + clienteight >= scrollHeight - 5) {
+    loadPosts();
+  }
+};
+
+window.addEventListener('scroll', handleScroll);
+
+loadosts();
