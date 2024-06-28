d422c84209ab61bfacba4ea9904d00e9ebf1115a
Samrat Bose <samratofficial2009@gmail.com>
2024-06-28 11:40:33 +0530

    update

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 let page = 1;
 const pageSize = 5;
+
 const newsContainer = document.getElementById('news-container');
 const loading = document.getElementById('loading');
 
 const apiKey = 'fbb23737fde64638a0cc9c652a6b255a'; 
-const apiUrl = `https://newsapi.org/v2/top-headlines?category=&language=en&pageSize=${pageSize}&apiKey=${apiKey}&page=`;
+const apiUrl = `https://newsapi.org/v2/top-headlines?category=technology&pageSize=${pageSize}&apiKey=${apiKey}&page=`;
 
 const loadArticles = async () => {
   loading.style.display = 'block';
@@ -17,30 +18,31 @@ const loadArticles = async () => {
 
     data.articles.forEach(article => {
       const articleElement = document.createElement('div');
-      articleElent.classList.add('article');
+      articleElement.classList.add('article');
       articleElement.innerHTML = `
         <h2>${article.title}</h2>
         <p>${article.description}</p>
-        <p><a href="${article.url}" target="_blank">Read more</a></p>`;
+        <p><a href="${article.url}" target="_blank">Read more</a></p>
+      `;
       newsContainer.appendChild(articleElement);
     });
 
     loading.style.display = 'none';
     page += 1;
   } catch (error) {
-    console.error('Error fetching articles:', error);
+    console.error('Error fetching articles:', error.message);
     loading.style.display = 'none';
   }
 };
 
 const handleScroll = () => {
-  const { scrollTop, screight, clientHeight } = document.documentElement;
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
 
   if (scrollTop + clientHeight >= scrollHeight - 5) {
     loadArticles();
   }
 };
 
-window.addEventListener('scroll', handlescroll);
+window.addEventListener('scroll', handleScroll);
 
 loadArticles();
